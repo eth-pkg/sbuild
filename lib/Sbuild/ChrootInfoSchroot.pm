eth-pkg/sbuild

commit 8a3e12c3edf768ddeb66b29157d3cf1a7ad3d48d

Replaced all unicode-printed chars with plain ASCII (Closes: #765886, #665847)
Yes, it is nearly 2016 and Debian has been using unicode for over a
decade. Yes, everybody should be using it by now. What made me (josch)
remove unicode characters and revert to back to ASCII is that the only
advantage of using unicode characters is:

 - Slightly more pretty output. I'd say the improvement is so little
   that I would call it negligible.

But on the other hand, there is a disadvantage which I'd call much more
practical than maybe having a more visually pleasing output:

 - sbuild logs are not necessarily only consumed by users of Debian or
   transmitted with technologies we ship with Debian. They might be
   copy-pasted to mail messages (read by people with very different clients),
   personal book keeping programs and web interfaces of services with no
   unicode support. Yes, all these places *should* know unicode but sbuild
   logs should also not become less readable if they don't.

Given that there is really no hard *requirement* for keeping the unicode
characters for pretty drawing (in contrast to a program which, for
example, handles asian script), I'm applying Dima's patch.

The reason why ASCII output is now the default instead of adding a
--ascii option is, that at the time where the build is done, one does
not know which places the build logs might reach.  The same reasoning
goes for why unicode support is not enabled or disabled depending on the
capabilities of the terminal sbuild outputs to.  Instead, if one is very
sure that the log will never be consumed by or transmitted through a
medium without unicode support, then there could be a --pretty-unicode
option in the future which explicitly enables the pretty boxes again.

lib/Sbuild/ChrootInfoSchroot.pm
@@ -61,15 +61,15 @@ sub get_info_from_stream {
 
 	last if ! $_;
 
-	if (/\s*─── Chroot ───/ &&
+	if (/\s*--- Chroot ---/ &&
 	    $tmp{'Namespace'} eq "") {
 	    $tmp{'Namespace'} = 'chroot';
 	}
-	if (/\s*─── Session ───/ &&
+	if (/\s*--- Session ---/ &&
 	    $tmp{'Namespace'} eq "") {
 	    $tmp{'Namespace'} = 'session';
 	}
-	if (/\s*─── Source ───/ &&
+	if (/\s*--- Source ---/ &&
 	    $tmp{'Namespace'} eq "") {
 	    $tmp{'Namespace'} = 'source';
 	}

lib/Sbuild.pm
@@ -139,12 +139,12 @@ sub dump_file ($) {
     if (-r "$file" &&
 	open(SOURCES, "<$file")) {
 
-	print "   ┌────────────────────────────────────────────────────────────────────────\n";
+	print "   +------------------------------------------------------------------------\n";
 	while (<SOURCES>) {
 	    chomp;
-	    print "   │$_\n";
+	    print "   |$_\n";
 	}
-	print "   └────────────────────────────────────────────────────────────────────────\n";
+	print "   +------------------------------------------------------------------------\n";
 	close(SOURCES) or print "Failed to close $file\n";
     } else {
 	print "W: Failed to open $file\n";
@@ -212,7 +212,7 @@ sub check_packages ($$) {
 	}
 
 	print "DELETE             ADD\n";
-	print "──────────────────────────────────────\n";
+	print "--------------------------------------\n";
 	my $i = 0;
 	my $j = 0;
 
@@ -284,7 +284,7 @@ sub usage_error ($$) {
     my $message = shift;
 
     print STDERR "E: $message\n";
-    print STDERR "I: Run “$program --help” to list usage example and all available options\n";
+    print STDERR "I: Run '$program --help' to list usage example and all available options\n";
     exit 1;
 }
 

lib/Sbuild/Base.pm
@@ -120,9 +120,9 @@ sub log_section {
     my $section = shift;
 
     $self->log("\n");
-    $self->log('╔', '═' x 78, '╗', "\n");
-    $self->log('║', " $section ", ' ' x (80 - length($section) - 4), '║', "\n");
-    $self->log('╚', '═' x 78, '╝', "\n\n");
+    $self->log('+', '=' x 78, '+', "\n");
+    $self->log('|', " $section ", ' ' x (80 - length($section) - 4), '|', "\n");
+    $self->log('+', '=' x 78, '+', "\n\n");
 }
 
 sub log_subsection {
@@ -130,9 +130,9 @@ sub log_subsection {
     my $section = shift;
 
     $self->log("\n");
-    $self->log('┌', '─' x 78, '┐', "\n");
-    $self->log('│', " $section ", ' ' x (80 - length($section) - 4), '│', "\n");
-    $self->log('└', '─' x 78, '┘', "\n\n");
+    $self->log('+', '-' x 78, '+', "\n");
+    $self->log('|', " $section ", ' ' x (80 - length($section) - 4), '|', "\n");
+    $self->log('+', '-' x 78, '+', "\n\n");
 }
 
 sub log_subsubsection {
@@ -141,13 +141,13 @@ sub log_subsubsection {
 
     $self->log("\n");
     $self->log("$section\n");
-    $self->log('─' x (length($section)), "\n\n");
+    $self->log('-' x (length($section)), "\n\n");
 }
 
 sub log_sep {
     my $self = shift;
 
-    $self->log('─' x 80, "\n");
+    $self->log('-' x 80, "\n");
 }
 
 1;

lib/Sbuild/Build.pm
@@ -2267,7 +2267,7 @@ sub open_build_log {
 	    # process in log stream).
 	    if (m/$filter_regex/) {
 		($text,$replacement)=($1,$2);
-		$replacement = "«$replacement»";
+		$replacement = "<<$replacement>>";
 		push (@filter, [$text, $replacement]);
 		$_ = "I: NOTICE: Log filtering will replace '$text' with '$replacement'\n";
 	    } elsif (m/$colour_regex/) {

lib/Sbuild/Conf.pm
@@ -571,7 +571,7 @@ sub setup ($) {
 	    VARNAME => 'lock_interval',
 	    GROUP => 'Build timeouts',
 	    DEFAULT => 5,
-	    HELP => 'Lock wait interval (seconds).  Maximum wait time is (max_lock_trys × lock_interval).'
+	    HELP => 'Lock wait interval (seconds).  Maximum wait time is (max_lock_trys x lock_interval).'
 	},
 	'CHROOT_MODE'				=> {
 	    TYPE => 'STRING',

commit 297dc84acd5fcb1f981b171cff6e7b63624db0c1

Fix regex parsing `schroot --info` output (closes: #801572)
 - this was a regression introduced by commit 8a3e12c3
 - this commit fixes the regression and also improves the regex

lib/Sbuild/ChrootInfoSchroot.pm
@@ -61,27 +61,15 @@ sub get_info_from_stream {
 
 	last if ! $_;
 
-	if (/\s*--- Chroot ---/ &&
+	if (/^\s*(───|---) Chroot \1$/ &&
 	    $tmp{'Namespace'} eq "") {
 	    $tmp{'Namespace'} = 'chroot';
 	}
-	if (/\s*--- Session ---/ &&
+	if (/^\s*(───|---) Session \1$/ &&
 	    $tmp{'Namespace'} eq "") {
 	    $tmp{'Namespace'} = 'session';
 	}
-	if (/\s*--- Source ---/ &&
-	    $tmp{'Namespace'} eq "") {
-	    $tmp{'Namespace'} = 'source';
-	}
-	if (/\s*--- Chroot ---/ &&
-	    $tmp{'Namespace'} eq "") {
-	    $tmp{'Namespace'} = 'chroot';
-	}
-	if (/\s*--- Session ---/ &&
-	    $tmp{'Namespace'} eq "") {
-	    $tmp{'Namespace'} = 'session';
-	}
-	if (/\s*--- Source ---/ &&
+	if (/^\s*(───|---) Source \1$/ &&
 	    $tmp{'Namespace'} eq "") {
 	    $tmp{'Namespace'} = 'source';
 	}