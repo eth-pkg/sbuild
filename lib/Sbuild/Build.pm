eth-pkg/sbuild

commit 9162dd0d727facb2ebab0ab7008603c5fa8c36a1

Check architecture wildcard matching with dpkg inside the chroot to avoid situations in which dpkg outside the chroot doesn't know about a new architecture yet (closes: #691539)

lib/Sbuild/Build.pm
@@ -1118,7 +1118,23 @@ sub fetch_source_files {
     } else {
 	my $valid_arch;
 	for my $a (split(/\s+/, $dscarchs)) {
-	    if (Dpkg::Arch::debarch_is($host_arch, $a)) {
+	    my $command = <<"EOF";
+		use strict;
+		use warnings;
+		use Dpkg::Arch;
+		if (Dpkg::Arch::debarch_is($host_arch, $a)) {
+		    exit 0;
+		}
+		exit 1;
+EOF
+	    $self->get('Session')->run_command(
+		{ COMMAND => ['perl',
+			'-e',
+			$command],
+		    USER => 'root',
+		    PRIORITY => 0,
+		    DIR => '/' });
+	    if ($? == 0) {
 		$valid_arch = 1;
 		last;
 	    }

commit 2deb0b121d8ca5dffcdf3b50cefd4a59bbc4554d

Fix quoting when checking architectures (closes: #809175)
The variables got interpolated into $command without any quoting,
essentially becoming barewords.

Regression introduced in 9162dd0d727facb2ebab0ab7008603c5fa8c36a1

lib/Sbuild/Build.pm
@@ -1139,7 +1139,7 @@ sub fetch_source_files {
 		use strict;
 		use warnings;
 		use Dpkg::Arch;
-		if (Dpkg::Arch::debarch_is($host_arch, $a)) {
+		if (Dpkg::Arch::debarch_is('$host_arch', '$a')) {
 		    exit 0;
 		}
 		exit 1;