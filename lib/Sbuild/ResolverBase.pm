1159497b26aabbb3380ac3475c11aff556ca1de2
Johannes 'josch' Schauer <josch@mister-muffin.de>
2018-07-01 16:05:39 +0200

    lib/Sbuild/ResolverBase.pm: clean /var/cache/apt/archive/*.deb after package installation
    Closes: #861762

--- lib/Sbuild/ResolverBase.pm
+++ lib/Sbuild/ResolverBase.pm
@@ -115,6 +115,8 @@ sub setup {
     print $F qq(APT::AutoRemove::SuggestsImportant "false";\n);
     print $F qq(APT::AutoRemove::RecommendsImportant "false";\n);
     print $F qq(Acquire::Languages "none";\n); # do not download translations
+    # remove packages from /var/cache/apt/archive/*.deb after installation
+    print $F qq(APT::Keep-Downloaded-Packages "false";\n);
 
     if ($self->get('Split')) {
 	print $F "Dir \"$chroot_dir\";\n";
